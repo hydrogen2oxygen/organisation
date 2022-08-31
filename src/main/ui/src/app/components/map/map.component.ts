968b537276e368f96d6d464ab23b0dda85d49fb7
hydrogen2oxygen <luxuspeter@googlemail.com>
2022-08-31 16:08:37 +0200

    Map OpenLayers integration

--- src/main/ui/src/app/components/map/map.component.ts
+++ src/main/ui/src/app/components/map/map.component.ts
@@ -1,4 +1,11 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import olMap from 'ol/Map';
+import View from 'ol/View';
+import XYZ from 'ol/source/XYZ';
+import VectorSource from "ol/source/Vector";
+import TileLayer from "ol/layer/Tile";
+import {OSM} from "ol/source";
+import VectorLayer from "ol/layer/Vector";
 
 @Component({
   selector: 'app-map',
@@ -7,9 +14,45 @@ import { Component, OnInit } from '@angular/core';
 })
 export class MapComponent implements OnInit {
 
+  map: olMap | null = null;
+  view: View = new View();
+  source = new VectorSource();
+  interaction: any = null;
+
   constructor() { }
 
   ngOnInit(): void {
+
+    const osmLayer = new TileLayer({
+      source: new OSM(),
+    });
+    osmLayer?.getSource()?.setAttributions([]);
+
+    const vectorLayer = new VectorLayer({
+      source: this.source
+    });
+
+    const xyzLayer = new TileLayer({
+      source: new XYZ({
+        url: 'http://tile.osm.org/{z}/{x}/{y}.png'
+      })
+    });
+
+    this.view = new View({
+      center: [-472202, 7530279],
+      zoom: 12
+    });
+    this.map = new olMap({
+      layers: [
+        osmLayer,
+        vectorLayer
+      ],
+      view: this.view,
+      controls: []
+    });
   }
 
+  ngAfterViewInit(): void {
+    this.map?.setTarget('map');
+  }
 }

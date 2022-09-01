9483b74318361c2ee3f70e2c7e064e83ff3e0c83
hydrogen2oxygen <luxuspeter@googlemail.com>
2022-09-01 10:39:04 +0200

    Vis-network works WIP

--- src/main/ui/src/app/components/diagram/diagram.component.ts
+++ src/main/ui/src/app/components/diagram/diagram.component.ts
@@ -1,4 +1,7 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import { Network } from "vis-network/peer/esm/vis-network";
+import { DataSet } from "vis-data/peer/esm/vis-data"
+import {EdgeVis, NodeVis} from "../../domains/NetworkingDomains";
 
 @Component({
   selector: 'app-diagram',
@@ -7,9 +10,34 @@ import { Component, OnInit } from '@angular/core';
 })
 export class DiagramComponent implements OnInit {
 
-  constructor() { }
+  constructor() {}
 
   ngOnInit(): void {
+    let nodes = new DataSet<NodeVis>([
+      { id: 1, label: "Node 1" },
+      { id: 2, label: "Node 2" },
+      { id: 3, label: "Node 3" },
+      { id: 4, label: "Node 4" },
+      { id: 5, label: "Node 5" },
+    ]);
+
+    let edges: DataSet<EdgeVis> = new DataSet([
+      { id: 1, from: 1, to: 3 },
+      { id: 2, from: 1, to: 2 },
+      { id: 3, from: 2, to: 4 },
+      { id: 4, from: 2, to: 5 }
+    ]);
+
+    // create a network
+    let container = document.getElementById("mynetwork");
+    let data = {
+      nodes: nodes,
+      edges: edges
+    };
+    let options = {};
+    if (container) {
+      let network = new Network(container, data, options);
+    }
   }
 
 }
---
+++ src/main/ui/src/app/domains/NetworkingDomains.ts
@@ -0,0 +1,10 @@
+export interface NodeVis {
+  id: number;
+  label: string;
+}
+
+export interface EdgeVis {
+  id: number;
+  from: number;
+  to: number;
+}
